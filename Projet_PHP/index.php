2523744f4f72cef9be82b5423bee1f8830a777d8
mr-jake <mr-jake@hotmail.fr>
2013-06-17 15:48:11 +0000

    Ajout de sidebar,footer.

--- Projet_PHP/index.php
+++ Projet_PHP/index.php
@@ -50,11 +50,60 @@
               </div>        
     </nav>
     <body>
+        <div class="row-fluid">
+
+            <div class="span9">
+                <div class="modal-body">
+                    <p>LoremLorem ipsum dolor sit amet, consectetur adipiscing elit. In a lacinia velit, sit amet commodo augue. Nam nulla turpis, blandit a malesuada at, scelerisque at lorem. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi pharetra euismod est eu auctor. Etiam ut orci commodo, malesuada magna quis, commodo erat. Proin ut tellus eget velit fringilla placerat ac vel justo. Duis fermentum dolor metus, sit amet faucibus dolor egestas non. Ut nec nisl auctor, commodo felis at, vulputate diam. Curabitur imperdiet velit sed ante venenatis, eu pellentesque erat tincidunt. Sed nec vehicula dolor. Sed a accumsan dolor. Praesent nec turpis feugiat, malesuada neque id, ultrices mi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam consectetur lacus enim, id ullamcorper erat interdum ac. Pellentesque quis enim vel augue ornare tincidunt et mollis nulla. Nullam porta felis sed convallis sagittis.
+Integer malesuada blandit metus, ut molestie erat vehicula quis. Morbi vulputate, leo quis rhoncus congue, dolor tortor volutpat magna, et placerat erat dolor at ligula. In ornare nibh vel porta dapibus. Cras pellentesque imperdiet imperdiet. Cras dapibus lacus orci, non laoreet nulla malesuada quis. Nam venenatis vulputate mauris, sed dictum lectus ultrices et. Nulla ac libero suscipit, feugiat tortor in, hendrerit risus. Nunc ipsum justo, luctus non lorem et, dictum semper dui. Morbi sit amet lacus aliquam, lobortis lacus sed, aliquam massa. Aliquam sit amet leo ac massa feugiat lobortis. Nam eget metus arcu. Praesent sit amet purus id dui pharetra malesuada.
+Nullam sodales dignissim justo, ac ullamcorper velit posuere nec. Cras id dignissim dui, a cursus ligula. Etiam eu mauris porta, eleifend erat sit amet, eleifend nisl. Maecenas elementum sodales pellentesque. Aenean in nibh et nibh faucibus molestie a a risus. Mauris vitae mauris enim. Vestibulum consequat, velit posuere convallis interdum, massa erat vestibulum diam, quis porttitor tellus lectus eu massa. Aenean a sem vel sapien rutrum feugiat at at diam. Nulla interdum, ipsum nec sollicitudin facilisis, odio enim tincidunt orci, vitae vulputate tellus metus sit amet purus. Fusce et adipiscing sem. Duis at nunc feugiat, consectetur purus eu, ullamcorper orci. Phasellus molestie posuere tortor, at consectetur arcu. Proin elementum scelerisque dolor, ac dignissim ligula bibendum ac.
+Aenean porta laoreet odio, non fermentum quam iaculis sed. Duis ut eleifend nisi. Nulla euismod lobortis iaculis. Aenean sit amet pharetra nibh, at fringilla ipsum. Pellentesque euismod elit ac iaculis laoreet. Suspendisse eu vestibulum sapien. Pellentesque eu malesuada purus. Sed egestas lacus sit amet elementum pretium. Pellentesque lectus magna, tristique nec aliquet nec, malesuada id velit. Etiam vitae diam a tortor fringilla malesuada lobortis vitae tortor. Ut id ipsum sapien. Pellentesque consectetur interdum velit, vel tincidunt justo aliquam nec. Nam quis libero nec ipsum congue pretium. Nam sit amet dui purus. Nunc pulvinar turpis sagittis, faucibus lacus ac, ullamcorper arcu.
+Aliquam eu vehicula enim, eget consequat nibh. Suspendisse potenti. Nam in eros in quam rhoncus venenatis sed ut nisi. In hac habitasse platea dictums ipsum dolor sit amet, consectetur adipiscing elit. In a lacinia velit, sit amet commodo augue. Nam nulla turpis, blandit a malesuada at, scelerisque at lorem. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi pharetra euismod est eu auctor. Etiam ut orci commodo, malesuada magna quis, commodo erat. Proin ut tellus eget velit fringilla placerat ac vel justo. Duis fermentum dolor metus, sit amet faucibus dolor egestas non. Ut nec nisl auctor, commodo felis at, vulputate diam. Curabitur imperdiet velit sed ante venenatis, eu pellentesque erat tincidunt. Sed nec vehicula dolor. Sed a accumsan dolor. Praesent nec turpis feugiat, malesuada neque id, ultrices mi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam consectetur lacus enim, id ullamcorper erat interdum ac. Pellentesque quis enim vel augue ornare tincidunt et mollis nulla. Nullam porta felis sed convallis sagittis.
+Integer malesuada blandit metus, ut molestie erat vehicula quis. Morbi vulputate, leo quis rhoncus congue, dolor tortor volutpat magna, et placerat erat dolor at ligula. In ornare nibh vel porta dapibus. Cras pellentesque imperdiet imperdiet. Cras dapibus lacus orci, non laoreet nulla malesuada quis. Nam venenatis vulputate mauris, sed dictum lectus ultrices et. Nulla ac libero suscipit, feugiat tortor in, hendrerit risus. Nunc ipsum justo, luctus non lorem et, dictum semper dui. Morbi sit amet lacus aliquam, lobortis lacus sed, aliquam massa. Aliquam sit amet leo ac massa feugiat lobortis. Nam eget metus arcu. Praesent sit amet purus id dui pharetra malesuada.
+Nullam sodales dignissim justo, ac ullamcorper velit posuere nec. Cras id dignissim dui, a cursus ligula. Etiam eu mauris porta, eleifend erat sit amet, eleifend nisl. Maecenas elementum sodales pellentesque. Aenean in nibh et nibh faucibus molestie a a risus. Mauris vitae mauris enim. Vestibulum consequat, velit posuere convallis interdum, massa erat vestibulum diam, quis porttitor tellus lectus eu massa. Aenean a sem vel sapien rutrum feugiat at at diam. Nulla interdum, ipsum nec sollicitudin facilisis, odio enim tincidunt orci, vitae vulputate tellus metus sit amet purus. Fusce et adipiscing sem. Duis at nunc feugiat, consectetur purus eu, ullamcorper orci. Phasellus molestie posuere tortor, at consectetur arcu. Proin elementum scelerisque dolor, ac dignissim ligula bibendum ac.
+Aenean porta laoreet odio, non fermentum quam iaculis sed. Duis ut eleifend nisi. Nulla euismod lobortis iaculis. Aenean sit amet pharetra nibh, at fringilla ipsum. Pellentesque euismod elit ac iaculis laoreet. Suspendisse eu vestibulum sapien. Pellentesque eu malesuada purus. Sed egestas lacus sit amet elementum pretium. Pellentesque lectus magna, tristique nec aliquet nec, malesuada id velit. Etiam vitae diam a tortor fringilla malesuada lobortis vitae tortor. Ut id ipsum sapien. Pellentesque consectetur interdum velit, vel tincidunt justo aliquam nec. Nam quis libero nec ipsum congue pretium. Nam sit amet dui purus. Nunc pulvinar turpis sagittis, faucibus lacus ac, ullamcorper arcu.
+Aliquam eu vehicula enim, eget consequat nibh. Suspendisse potenti. Nam in eros in quam rhoncus venenatis sed ut nisi. In hac habitasse platea dictumst. Maecenas quis consequat dui. Phasellus vel ipsum tincidunt, hendrerit erat accumsan, tempus felis. Nulla facilisi. Nam imperdiet semper enim, a convallis augue auctor non. Phasellus consectetur eros mi, malesuada viverra eros eleifend sit amet. Ut ornare auctor commodo. Nunc dictum luctus purus sit amet adipiscing. Pellentesque suscipit venenatis malesuada. Aliquam erat volutpat. Vivamus pellentesque ipsum non est aliquet vulputate. Sed posuere vel dolor non vulputate.</p>
+                </div>
+                                        <footer class="modal-footer">
+                <p>&copy; Copyright 2013 EXI@STORE Groupe: Clement Valentin Lucien</p>
+            </footer>   
+            </div>
+            <div class="span3">
+                <div class="sidebar-nav-fixed offset9">
+                    <ul class="nav nav-list">
+                        <li class="nav-header">Mon Compte</li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                            <li class="nav-header">Panier</li>
+                        <div class="well">
+                            <li><a href="#">Link</a></li>
+                            <li><a href="#">Link</a></li>
+                            <li><a href="#">Link</a></li>
+                            <li><a href="#">Link</a></li>
+                            <li><a href="#">Link</a></li>
+                            <button href="" class=" btn_panier btn-primary">Voir tous le panier</button>
+                        </div>
+                        <li class="nav-header">Publicite</li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li class="nav-header">Partenaires</li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                    </ul>
+                </div><!--/.well --> 
+            </div><!-- /.span -->
+
+        </div><!-- /.row-fluid -->
  
         <?php
 
         ?>
     </body>
+
         <!-- Le javascript
             ================================================== -->
             <!-- Placed at the end of the document so the pages load faster -->
