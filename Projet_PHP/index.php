fbc032446907b8fac3352923db3dbba0b8a16886
mr-jake <mr-jake@hotmail.fr>
2013-06-17 12:32:17 +0000

    Ajout des text box de login, renormalisation du dossier,dropdown(catalogue)

---
+++ Projet_PHP/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+        <!-- Le styles -->
+        <link href="../assets/css/bootstrap.css" rel="stylesheet">
+        <style>
+          body {
+            padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
+          }
+        </style>
+        <title></title>
+    </head>
+    <nav>
+        <!-- La barre de navigation -->
+           <div class="navbar navbar-fixed-top">
+                <div class="navbar-inner">
+
+                  <div class="container">
+                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                    </a>
+                    <a class="brand" href="#">Exi@Store</a>
+                    <div class="nav-collapse">
+                      <ul class="nav">
+                        <li class="active"><a href="#">Accueil</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Catalogue <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                          <li><a href="#">CD</a></li>
+                          <li><a href="#">DVD</a></li>
+                          <li><a href="#">Recherche</a></li>
+                        </ul>
+                    </li>
+                      </ul>
+
+                        <div class="navbar-form pull-right">
+                        <input type="text" class="txtbx_login" placeholder="Nom d'utilisateur">
+                        <input type="password" class="txtbx_login" placeholder="Mot de Passe">
+                        <button type="submit" class="submit_login" class="btn">Se Connecter</button>
+                        </div>
+                    </div><!--/.nav-collapse -->
+                     
+                  </div>
+                </div>
+       
+              </div>        
+    </nav>
+    <body>
+ 
+        <?php
+
+        ?>
+    </body>
+        <!-- Le javascript
+            ================================================== -->
+            <!-- Placed at the end of the document so the pages load faster -->
+            <script src="js/jquery.js"></script>
+            <script src="js/bootstrap.js"></script>
+</html>
