a7f165f87c7d23e02a232ad01ae68a29f572b204
mr-jake@hotmail.fr <mr-jake@hotmail.fr@5e3f62cd-2a45-db39-c6d4-f4eed0aa111a>
2013-06-18 07:29:43 +0000

    Ajout du carousel

--- trunk/Projet_PHP/index.php
+++ trunk/Projet_PHP/index.php
@@ -54,6 +54,21 @@
 
             <div class="span9">
                 <div class="modal-body">
+                    <div id="myCarousel" class="carousel slide">
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+  <!-- Carousel items -->
+  <div class="carousel-inner">
+    <div class="active item"><img alt="" src="img/carousel_first.jpg"/></div>
+    <div class="item"><img alt="" src="img/carousel_second.jpg"/></div>
+  </div>
+  <!-- Carousel nav -->
+  <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+  <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+</div>
                     <p>LoremLorem ipsum dolor sit amet, consectetur adipiscing elit. In a lacinia velit, sit amet commodo augue. Nam nulla turpis, blandit a malesuada at, scelerisque at lorem. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi pharetra euismod est eu auctor. Etiam ut orci commodo, malesuada magna quis, commodo erat. Proin ut tellus eget velit fringilla placerat ac vel justo. Duis fermentum dolor metus, sit amet faucibus dolor egestas non. Ut nec nisl auctor, commodo felis at, vulputate diam. Curabitur imperdiet velit sed ante venenatis, eu pellentesque erat tincidunt. Sed nec vehicula dolor. Sed a accumsan dolor. Praesent nec turpis feugiat, malesuada neque id, ultrices mi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam consectetur lacus enim, id ullamcorper erat interdum ac. Pellentesque quis enim vel augue ornare tincidunt et mollis nulla. Nullam porta felis sed convallis sagittis.
 Integer malesuada blandit metus, ut molestie erat vehicula quis. Morbi vulputate, leo quis rhoncus congue, dolor tortor volutpat magna, et placerat erat dolor at ligula. In ornare nibh vel porta dapibus. Cras pellentesque imperdiet imperdiet. Cras dapibus lacus orci, non laoreet nulla malesuada quis. Nam venenatis vulputate mauris, sed dictum lectus ultrices et. Nulla ac libero suscipit, feugiat tortor in, hendrerit risus. Nunc ipsum justo, luctus non lorem et, dictum semper dui. Morbi sit amet lacus aliquam, lobortis lacus sed, aliquam massa. Aliquam sit amet leo ac massa feugiat lobortis. Nam eget metus arcu. Praesent sit amet purus id dui pharetra malesuada.
 Nullam sodales dignissim justo, ac ullamcorper velit posuere nec. Cras id dignissim dui, a cursus ligula. Etiam eu mauris porta, eleifend erat sit amet, eleifend nisl. Maecenas elementum sodales pellentesque. Aenean in nibh et nibh faucibus molestie a a risus. Mauris vitae mauris enim. Vestibulum consequat, velit posuere convallis interdum, massa erat vestibulum diam, quis porttitor tellus lectus eu massa. Aenean a sem vel sapien rutrum feugiat at at diam. Nulla interdum, ipsum nec sollicitudin facilisis, odio enim tincidunt orci, vitae vulputate tellus metus sit amet purus. Fusce et adipiscing sem. Duis at nunc feugiat, consectetur purus eu, ullamcorper orci. Phasellus molestie posuere tortor, at consectetur arcu. Proin elementum scelerisque dolor, ac dignissim ligula bibendum ac.
@@ -76,7 +91,7 @@ Aliquam eu vehicula enim, eget consequat nibh. Suspendisse potenti. Nam in eros 
                         <li><a href="#">Link</a></li>
                         <li><a href="#">Link</a></li>
                         <li><a href="#">Link</a></li>
-                            <li class="nav-header">Panier</li>
+                            <li class="nav-header"> Panier   <i class="icon-shopping-cart"></i></li>
                         <div class="well">
                             <li><a href="#">Link</a></li>
                             <li><a href="#">Link</a></li>
