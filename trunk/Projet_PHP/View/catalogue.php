36d00e1a05553bf872edf2ce122f69a05efee496
valentin.lafranca@gmail.com <valentin.lafranca@gmail.com@5e3f62cd-2a45-db39-c6d4-f4eed0aa111a>
2013-06-20 07:45:54 +0000

    add catalogue

--- trunk/Projet_PHP/View/catalogue.php
+++ trunk/Projet_PHP/View/catalogue.php
@@ -5,3 +5,95 @@
  * and open the template in the editor.
  */
 ?>
+<div class="span9">
+                <div class="modal-body">
+                    <div id="myCarousel" class="carousel slide">
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+  <!-- Carousel items -->
+  <div class="carousel-inner">
+    <div class="active item"><img alt="" src="<?php echo __SITE_URL; ?>/img/carousel_first.jpg"/>        <div class="carousel-caption">
+            <p>Seulement sur notre site.</p>
+         </div></div>
+
+    <div class="item"><img alt="" src="<?php echo __SITE_URL; ?>/img/carousel_first.jpg"/>        <div class="carousel-caption">
+            <p>A partir du 18 Juin au 31 Juin.</p>
+         </div></div>
+
+    <div class="item"><img alt="" src="<?php echo __SITE_URL; ?>/img/carousel_first.jpg"/>        <div class="carousel-caption">
+            <p>Seulement pour vous.</p>
+         </div>
+    </div>
+
+  </div>
+  <!-- Carousel nav -->
+  <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+  <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+</div>
+        <!-- BODY  -->
+        <div class="well" >
+            <div class="article_catalogue well_blanc">
+                <img class="img_catalogue"  alt="" src="<?php echo __SITE_URL; ?>/img/catalogue/article1.jpg" href="#"/>
+                <p> Album Black Sabbath 13 (2013) <span style="color:red;">24 Euros</span></p>
+                                                <button href=""  name="btn_add_panier" class="btn_voir_article btn-primary">Voir</button>
+            </div>
+                        <div class="article_catalogue well_blanc">
+                <img class="img_catalogue"  alt="" src="img/catalogue/article1.jpg" href="#"/>
+                <p> Album Black Sabbath 13 (2013) <span style="color:red;">24 Euros</span></p>
+                                                <button href=""  name="btn_add_panier" class="btn_voir_article btn-primary">Voir</button>
+            </div>
+                        <div class="article_catalogue well_blanc">
+                <img class="img_catalogue"  alt="" src="img/catalogue/article1.jpg" href="#"/>
+                <p> Album Black Sabbath 13 (2013) <span style="color:red;">24 Euros</span></p>
+                                                <button href=""  name="btn_add_panier" class="btn_voir_article btn-primary">Voir</button>
+            </div>
+                        <div class="article_catalogue well_blanc">
+                <img class="img_catalogue"  alt="" src="img/catalogue/article1.jpg" href="#"/>
+                <p> Album Black Sabbath 13 (2013) <span style="color:red;">24 Euros</span></p>
+                                                <button href=""  name="btn_add_panier" class="btn_voir_article btn-primary">Voir</button>
+            </div>
+                        <div class="article_catalogue well_blanc">
+                <img class="img_catalogue"  alt="" src="img/catalogue/article1.jpg" href="#"/>
+                <p class="nom_article"> Album Black Sabbath 13 (2013) <span style="color:red;">24 Euros</span></p>
+                                                <button href=""  name="btn_add_panier" class="btn_voir_article btn-primary">Voir</button>
+            </div>
+                        <div class="article_catalogue well_blanc">
+                <img class="img_catalogue"  alt="" src="img/catalogue/article1.jpg" href="#"/>
+                <p> Album Black Sabbath 13 (2013) <span style="color:red;">24 Euros</span></p>
+                                                <button href=""  name="btn_add_panier" class="btn_voir_article btn-primary">Voir</button>
+            </div>
+                        <div class="article_catalogue well_blanc">
+                <img class="img_catalogue"  alt="" src="img/catalogue/article1.jpg" href="#"/>
+                <p> Album Black Sabbath 13 (2013) <span style="color:red;">24 Euros</span></p>
+                                                <button href=""  name="btn_add_panier" class="btn_voir_article btn-primary">Voir</button>
+            </div>
+                        <div class="article_catalogue well_blanc">
+                <img class="img_catalogue"  alt="" src="img/catalogue/article1.jpg" href="#"/>
+                <p> Album Black Sabbath 13 (2013) <span style="color:red;">24 Euros</span></p>
+                                                <button href=""  name="btn_add_panier" class="btn_voir_article btn-primary">Voir</button>
+            </div>
+                        <div class="article_catalogue well_blanc">
+                <img class="img_catalogue" alt="" src="img/catalogue/article1.jpg" href="#"/>
+                <p> Album Black Sabbath 13 (2013) <span style="color:red;">24 Euros</span></p>
+                                                <button href=""  name="btn_add_panier" class="btn_voir_article btn-primary">Voir</button>
+            </div>
+                        <div class="article_catalogue well_blanc">
+                <img class="img_catalogue"  alt="" src="img/catalogue/article1.jpg" href="#"/>
+                <p> Album Black Sabbath 13 (2013) <span style="color:red;">24 Euros</span></p>
+                                                <button href=""  name="btn_add_panier" class="btn_voir_article btn-primary">Voir</button>
+            </div>
+                        <div class="article_catalogue well_blanc">
+                <img class="img_catalogue"  alt="" src="img/catalogue/article1.jpg" href="#"/>
+                <p> Album Black Sabbath 13 (2013) <span style="color:red;">24 Euros</span></p>
+                                                <button href=""  name="btn_add_panier" class="btn_voir_article btn-primary">Voir</button>
+            </div>
+            
+        </div>
+                </div>
+                                        <footer class="modal-footer">
+                <p>&copy; Copyright 2013 EXI@STORE Groupe: Clement Valentin Lucien</p>
+            </footer>   
+            </div>
