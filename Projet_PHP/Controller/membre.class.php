4c8e9134ce0a73e8fab88283c9d6e76527dc86f1
lucien.arrio <lucien.arrio@gmail.com>
2013-06-24 14:21:27 +0000

    Finalisation de la gestion des infos persos des membres, et de l'administration des articles

--- Projet_PHP/Controller/membre.class.php
+++ Projet_PHP/Controller/membre.class.php
@@ -9,77 +9,100 @@ class membre_class extends router {
     }
 
     function index() {
-        if(isset($_SESSION['user']))
-        {
-           $tab = $this->registry->db->membre_pdo->RemplirChamps();
-           $_SESSION['user']['Email'] = $tab['Email'];
-           $_SESSION['user']['Mdp'] = $tab['Mdp'];
-           $_SESSION['user']['Nom'] = $tab['Nom'];
-           $_SESSION['user']['Prenom'] = $tab['Prenom'];
-           $_SESSION['user']['Adresse'] = $tab['Adresse'];
-           $_SESSION['user']['Ville'] = $tab['Ville'];
-           $_SESSION['user']['CP'] = $tab['CP'];
+        if (isset($_SESSION['user'])) {
+            $tab = $this->registry->db->membre_pdo->RemplirChamps();
+            $_SESSION['user']['Email'] = $tab['Email'];
+            $_SESSION['user']['Mdp'] = $tab['Mdp'];
+            $_SESSION['user']['Nom'] = $tab['Nom'];
+            $_SESSION['user']['Prenom'] = $tab['Prenom'];
+            $_SESSION['user']['Adresse'] = $tab['Adresse'];
+            $_SESSION['user']['Ville'] = $tab['Ville'];
+            $_SESSION['user']['CP'] = $tab['CP'];
             $this->registry->template->show('membre');
-            
-        }else{
-           
-        $this->registry->template->message = "Vous n'êtes pas connecté";
+        } else {
+
+            $this->registry->template->message = "Vous n'êtes pas connecté";
             $this->registry->template->show('message');
         }
     }
-    function logout()
-    {
+
+    function logout() {
         session_unset();
         session_destroy();
         $this->registry->template->message = "Vous êtes déconnecté";
-            $this->registry->template->show('message');
+        $this->registry->template->show('message');
     }
-    
-    function modificationClient()
-    {
-        if (isset($_POST['Email'])){
+
+    function modificationClient() {
+        if (isset($_POST['Email'])) {
             $Email = ($_POST['Email']);
             $VerifEmail = ($_POST['VerifEmail']);
             $Nom = ($_POST['Nom']);
             $Prenom = ($_POST['Prenom']);
             $Mdp = ($_POST['Mdp']);
             $VerifMdp = ($_POST['VerifMdp']);
-            
-            if($Email != '' && $VerifEmail != '' && $Nom != '' && $Prenom != '' && $Mdp != '' && $VerifMdp != '')
-            {
+
+            if ($Email != '' && $VerifEmail != '' && $Nom != '' && $Prenom != '' && $Mdp != '' && $VerifMdp != '' && $Mdp === $VerifMdp && $Email === $VerifEmail) {
                 $resultat = $this->registry->db->membre_pdo->UpdateClient($Email, $Mdp, $Nom, $Prenom);
-                
+                echo "<script> alert('Succès') </script>";
+                $tab = $this->registry->db->membre_pdo->RemplirChamps();
+                $_SESSION['user']['Email'] = $tab['Email'];
+                $_SESSION['user']['Mdp'] = $tab['Mdp'];
+                $_SESSION['user']['Nom'] = $tab['Nom'];
+                $_SESSION['user']['Prenom'] = $tab['Prenom'];
+                $_SESSION['user']['Adresse'] = $tab['Adresse'];
+                $_SESSION['user']['Ville'] = $tab['Ville'];
+                $_SESSION['user']['CP'] = $tab['CP'];
+                $this->registry->template->show('membre');
+            } else {
+                echo "<script> alert('Champs non remplis ou mal renseignés') </script>";
+                $tab = $this->registry->db->membre_pdo->RemplirChamps();
+                $_SESSION['user']['Email'] = $tab['Email'];
+                $_SESSION['user']['Mdp'] = $tab['Mdp'];
+                $_SESSION['user']['Nom'] = $tab['Nom'];
+                $_SESSION['user']['Prenom'] = $tab['Prenom'];
+                $_SESSION['user']['Adresse'] = $tab['Adresse'];
+                $_SESSION['user']['Ville'] = $tab['Ville'];
+                $_SESSION['user']['CP'] = $tab['CP'];
+                $this->registry->template->show('membre');
             }
-            else 
-            {
-               $this->registry->template->message = "Champs non rempli";
-            }
-            $this->registry->template->message = "Succés";
-            $this->registry->template->show('message');
-        }else{
-        $this->registry->template->show('membre');
+        } else {
+            $this->registry->template->show('membre');
         }
     }
-    
-    function modificationAdresse()
-    {
-        if (isset($_POST['Adresse'])){
+
+    function modificationAdresse() {
+        if (isset($_POST['Adresse'])) {
             $Adresse = ($_POST['Adresse']);
             $Ville = ($_POST['Ville']);
             $CP = ($_POST['CP']);
-            
-            if($Adresse != '' && $Ville != '' && $CP != '')
-            {
+
+            if ($Adresse != '' && $Ville != '' && $CP != '' && is_numeric($CP)) {
                 $resultat = $this->registry->db->membre_pdo->UpdateAdresse($Adresse, $Ville, $CP);
+                echo "<script> alert('Succès') </script>";
+                $tab = $this->registry->db->membre_pdo->RemplirChamps();
+                $_SESSION['user']['Email'] = $tab['Email'];
+                $_SESSION['user']['Mdp'] = $tab['Mdp'];
+                $_SESSION['user']['Nom'] = $tab['Nom'];
+                $_SESSION['user']['Prenom'] = $tab['Prenom'];
+                $_SESSION['user']['Adresse'] = $tab['Adresse'];
+                $_SESSION['user']['Ville'] = $tab['Ville'];
+                $_SESSION['user']['CP'] = $tab['CP'];
+                $this->registry->template->show('membre');
+            } else {
+                echo "<script> alert('Champs non remplis ou mal renseigné') </script>";
+                $tab = $this->registry->db->membre_pdo->RemplirChamps();
+                $_SESSION['user']['Email'] = $tab['Email'];
+                $_SESSION['user']['Mdp'] = $tab['Mdp'];
+                $_SESSION['user']['Nom'] = $tab['Nom'];
+                $_SESSION['user']['Prenom'] = $tab['Prenom'];
+                $_SESSION['user']['Adresse'] = $tab['Adresse'];
+                $_SESSION['user']['Ville'] = $tab['Ville'];
+                $_SESSION['user']['CP'] = $tab['CP'];
+                $this->registry->template->show('membre');
             }
-            else 
-            {
-               $this->registry->template->message = "Champs non rempli";
-            }
-            $this->registry->template->message = "Succés";
-            $this->registry->template->show('message');
-        }else{
-        $this->registry->template->show('membre');
+        } else {
+            $this->registry->template->show('membre');
         }
     }
 
--- Projet_PHP/View/administration.php
+++ Projet_PHP/View/administration.php
@@ -51,14 +51,14 @@
             <table>
                 <tr>
                     <td>
-                        
+
                     </td>
                     <td>
 
 
 
-           
-                        
+
+
                         <form class="form-signin" action="<?php echo __SITE_URL ?>/administration/ajoutarticle" method="POST" name="formAjoutArticle" style="display:inline-block;">
                             <h2 class="form-signin-heading">Ajouter un article</h2>
                             Thème
@@ -68,24 +68,20 @@
                             Prix
                             <input type="text"  class="input-block-level" placeholder="Prix" name="Prix">
                             Type 
-                            
-                            </br>
-                            
-                            <!--<input type="text"  class="input-block-level" placeholder="Type" name="Type">-->
 
-                 
+                            </br>
+
+<!--<input type="text"  class="input-block-level" placeholder="Type" name="Type">-->
+
+
                             <select id="Type" name="Type">
                                 <?php
-// On affiche chaque entrée une à une
-                                foreach($ttype as $key => $donnees)
-                                {
+                                foreach ($ttype as $key => $donnees) {
                                     echo '<option selected value="' . $donnees['type'] . '">' . $donnees['type'] . '</option>'; //ou bien $donnees['monchamp']
-                                    
                                 }
-                                
                                 ?>
                             </select>
-                       
+
                             </br>
                             Titre
                             <input type="text"  class="input-block-level" placeholder="Titre" name="Titre">
@@ -104,33 +100,31 @@
 
                             <p></p>
                             <button class="btn btn-large btn-primary" type="submit" >Ajouter</button>
-                        </td>
-                        </form>  
+                    </td>
+                    </form>  
                     </div>
-                    <div class ="form-signin">
-                        <h2 class="form-signin-heading">Choisir un article</h2>
-                        <form  action="<?php echo __SITE_URL ?>/administration/afficherarticle" method="POST" id="formList2" style="display:inline-block;">
-                            <select onchange="document.getElementById('formList2').submit()" id="Titre" name="Titre">
-                                <?php
-// On affiche chaque entrée une à une
-                                foreach($Titre as $keyy => $donneess)
-                                {
-                                    echo '<option value="' . $donneess['Titre'] . '">' . $donneess['Titre'] . '</option>'; //ou bien $donnees['monchamp']
-                                    
-                                }
-                             
-                                ?>
-                                </select>
-                        </form>
-             
-                    </div>
-                   
-                    <td>
-                      <div class="form-signin">
-                         <h2 class="form-signin-heading">Modifier un article</h2>
-                         
+                <div class ="form-signin">
+                    <h2 class="form-signin-heading">Choisir un article</h2>
+                    <form  action="<?php echo __SITE_URL ?>/administration/afficherarticle" method="POST" id="formList2" style="display:inline-block;">
+                        <select onchange="document.getElementById('formList2').submit()" id="Titre" name="Titre">
+                        <?php
+                        // On affiche chaque entrée une à une
+                        foreach ($Titre as $keyy => $donneess) {
+
+                            echo '<option value="' . $donneess['Titre'] . '" >' . $donneess['Titre'] . '</option>';
+                        }
+                        ?>
+                        </select>
+                    </form>
+
+                </div>
+
+                <td>
+                    <div class="form-signin">
+                        <h2 class="form-signin-heading">Modifier un article</h2>
+
                         <form action="<?php echo __SITE_URL ?>/administration/modifierarticle" method="POST" name="formModifierArticle" style="display:inline-block;">
-                            
+
                             <input  name="ID_Article" id="ID_Article" type="text"  class="input-block-level" placeholder="ID" value="<?php echo $id_article; ?>">
                             Thème
                             <input type="text"  class="input-block-level" placeholder="Thème" name="Theme" value="<?php echo $theme; ?>">    
@@ -140,22 +134,19 @@
                             <input type="text"  class="input-block-level" placeholder="Prix" name="Prix" value="<?php echo $prix; ?>">
                             Type
                             </br>
-                            
+
                             <select style="width: 50%;" id="Type" name="Type">
-                                <?php
-// On affiche chaque entrée une à une
-                                foreach($ttype as $key => $donnees)
-                                {
-                                    echo '<option value="' . $donnees['type'] . '">' . $donnees['type'] . '</option>'; //ou bien $donnees['monchamp']
-                                    
-                                }
-                                
-                                ?>
+                            <?php
+                            // On affiche chaque entrée une à une
+                            foreach ($ttype as $key => $donnees) {
+                                echo '<option value="' . $donnees['type'] . '">' . $donnees['type'] . '</option>'; //ou bien $donnees['monchamp']
+                            }
+                            ?>
                             </select>
-                            
-                                <input type="text" style="width: 40%"  class="input-block-level" placeholder="Type" name="Type" disabled value="<?php echo $type;?>">
+
+                            <input type="text" style="width: 40%"  class="input-block-level" placeholder="Type" name="Type" disabled value="<?php echo $type; ?>">
                             Titre
-                            
+
                             <input type="text"  class="input-block-level" placeholder="Titre" name="Titre" value="<?php echo $titre; ?>">
                             Date d'édition
                             <input type="date"  class="input-block-level" placeholder="Date d'édition" name="Date_Edition" value="<?php echo $date_edition; ?>">
@@ -168,19 +159,19 @@
                             Stock
                             <input type="text"  class="input-block-level" placeholder="Stock" name="Stock" value="<?php echo $stock; ?>">
                             Statut
-                            <input type="text"  class="input-block-level" placeholder="Statut" name="Statut" value="<?php echo $statut ; ?>">
+                            <input type="text"  class="input-block-level" placeholder="Statut" name="Statut" value="<?php echo $statut; ?>">
 
-                         
+
                             <button class="btn btn-large btn-primary" type="submit" >Modifier</button>
                         </form>  
-                      </div>
-                    </td>
-                    <td>
-                        <div class="form-signin">
-                         <h2 class="form-signin-heading">Supprimer un article</h2>
-                         
+                    </div>
+                </td>
+                <td>
+                    <div class="form-signin">
+                        <h2 class="form-signin-heading">Supprimer un article</h2>
+
                         <form action="<?php echo __SITE_URL ?>/administration/supprimerarticle" method="POST" name="formSupprimerArticle" style="display:inline-block;">
-                          
+
                             <input  name="ID_Article2" id="ID_Article2"  type="text"  class="input-block-level" placeholder="ID"  value="<?php echo $id_article; ?>">
                             Thème
                             <input type="text"  class="input-block-level" placeholder="Thème" name="STheme" disabled value="<?php echo $theme; ?>">    
@@ -190,10 +181,10 @@
                             <input type="text"  class="input-block-level" placeholder="Prix" name="Prix" disabled value="<?php echo $prix; ?>">
                             Type
                             </br>
-                            
-                            <input type="text"  class="input-block-level" placeholder="Type" name="Type" disabled disabled value="<?php echo $type;?>">
+
+                            <input type="text"  class="input-block-level" placeholder="Type" name="Type" disabled disabled value="<?php echo $type; ?>">
                             Titre
-                            
+
                             <input type="text"  class="input-block-level" placeholder="Titre" name="Titre" disabled value="<?php echo $titre; ?>">
                             Date d'édition
                             <input type="date"  class="input-block-level" placeholder="Date d'édition" disabled name="Date_Edition" value="<?php echo $date_edition; ?>">
@@ -206,20 +197,20 @@
                             Stock
                             <input type="text"  class="input-block-level" placeholder="Stock" disabled name="Stock" value="<?php echo $stock; ?>">
                             Statut
-                            <input type="text"  class="input-block-level" placeholder="Statut" disabled name="Statut" value="<?php echo $statut ; ?>">
+                            <input type="text"  class="input-block-level" placeholder="Statut" disabled name="Statut" value="<?php echo $statut; ?>">
 
-                         
+
                             <button class="btn btn-large btn-primary" type="submit" >Supprimer</button>
                         </form>  
-                      </div>
-                    </td>
+                    </div>
+                </td>
                 </tr>
             </table>
         </center>
 
-<script>
-document.getElementById("ID_Article").style.display = 'none';
-document.getElementById("ID_Article2").style.display = 'none';
-</script>
+        <script>
+                            document.getElementById("ID_Article").style.display = 'none';
+                            document.getElementById("ID_Article2").style.display = 'none';
+        </script>
 
     </div> <!-- /container -->
