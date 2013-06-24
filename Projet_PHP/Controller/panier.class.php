5b6b597f2e31cddd878e771611974e7cf968543c
valentin.lafranca <valentin.lafranca@gmail.com>
2013-06-24 14:59:06 +0000

    -1 corrigé

--- Projet_PHP/Controller/panier.class.php
+++ Projet_PHP/Controller/panier.class.php
@@ -99,7 +99,7 @@ class panier_class extends router {
     function add() {
         if (isset($_POST['id'])) {                   //          id_article en id est = a la quantité
             $_SESSION['user']['panier'][$_POST['id']] = 1;
-            echo count($_SESSION['user']['panier']) - 1;
+            echo count($_SESSION['user']['panier']);
         }
     }
 
