289e183ff1c629a035b0c6432de6e5128f5615cc
lucien.arrio <lucien.arrio@gmail.com>
2013-06-24 11:39:52 +0000

    Ajouter / Modification / Suppression d'articles dans la partie Administration

--- Projet_PHP/Controller/administration.class.php
+++ Projet_PHP/Controller/administration.class.php
@@ -1,17 +1,261 @@
 <?php
 
-class administration_class extends router{
-    
-protected $registry;
+class administration_class extends router {
+
+    protected $registry;
 
     function __construct($registry) {
         $this->registry = $registry;
     }
-    
-    function index (){
-      
+
+    function index() {
+        if (isset($_SESSION['user']['user_admin'])) {
+            $this->registry->template->id_article = '';
+            $this->registry->template->theme = '';
+            $this->registry->template->img = '';
+            $this->registry->template->prix = '';
+            $this->registry->template->type = '';
+            $this->registry->template->titre = '';
+            $this->registry->template->date_edition = '';
+            $this->registry->template->description = '';
+            $this->registry->template->editeur = '';
+            $this->registry->template->seuil = '';
+            $this->registry->template->stock = '';
+            $this->registry->template->statut = '';
+            
+            
+            $reponse = $this->registry->db->administration_pdo->ListeDeroulanteType();
+            $reponse2 = $this->registry->db->administration_pdo->ListeDeroulanteArticle();
+            $reponse3 = $this->registry->db->administration_pdo->ListeDeroulanteArticle();
+            $this->registry->template->ttype = $reponse;
+            $this->registry->template->Titre = $reponse2;
+            $this->registry->template->TTitre = $reponse3;
+            
+            
+
+
             $this->registry->template->show('administration');
-              
+        } else {
+            $this->registry->template->message = "Vous n'êtes pas un admin";
+            $this->registry->template->show('message');
+        }
     }
+
+    function ajoutarticle() {
+        if (isset($_SESSION['user']['user_admin'])) {
+
+            $reponse = $this->registry->db->administration_pdo->ListeDeroulanteType();
+            $reponse2 = $this->registry->db->administration_pdo->ListeDeroulanteArticle();
+            $reponse3 = $this->registry->db->administration_pdo->ListeDeroulanteArticle();
+            $this->registry->template->type = $reponse;
+            $this->registry->template->Titre = $reponse2;
+            $this->registry->template->TTitre = $reponse3;
+
+
+            $Theme = ($_POST['Theme']);
+            $Image = ($_POST['Image']);
+            $Prix = ($_POST['Prix']);
+            $Type = ($_POST['Type']);
+            $Titre = ($_POST['Titre']);
+            $Date_Edition = ($_POST['Date_Edition']);
+            $Description = ($_POST['Description']);
+            $Editeur = ($_POST['Editeur']);
+            $Seuil = ($_POST['Seuil']);
+            $Stock = ($_POST['Stock']);
+            $Statut = ($_POST['Statut']);
+
+            if ($Theme != '' && $Image != '' && $Prix != '' && $Type != '' && $Titre != '' && $Date_Edition != '' && $Description != '' && $Editeur != '' && $Seuil != '' && $Stock != '' && $Statut != '') {
+
+                $resultat = $this->registry->db->administration_pdo->InsertAjoutArticle($Theme, $Image, $Prix, $Type, $Titre, $Date_Edition, $Description, $Editeur, $Seuil, $Stock, $Statut);
+               
+                $reponse = $this->registry->db->administration_pdo->ListeDeroulanteType();
+                $this->registry->template->type = $reponse;
+                echo "<script> alert('Succès') </script>";
+            $this->registry->template->id_article = '';
+            $this->registry->template->theme = '';
+            $this->registry->template->img = '';
+            $this->registry->template->prix = '';
+            $this->registry->template->type = '';
+            $this->registry->template->titre = '';
+            $this->registry->template->date_edition = '';
+            $this->registry->template->description = '';
+            $this->registry->template->editeur = '';
+            $this->registry->template->seuil = '';
+            $this->registry->template->stock = '';
+            $this->registry->template->statut = '';
+            
+            
+            $reponse = $this->registry->db->administration_pdo->ListeDeroulanteType();
+            $reponse2 = $this->registry->db->administration_pdo->ListeDeroulanteArticle();
+            $reponse3 = $this->registry->db->administration_pdo->ListeDeroulanteArticle();
+            $this->registry->template->ttype = $reponse;
+            $this->registry->template->Titre = $reponse2;
+            $this->registry->template->TTitre = $reponse3;
+                $this->registry->template->show('administration');
+            } else {
+                echo "<script> alert('Champs non remplis') </script>";
+               $this->registry->template->id_article = '';
+            $this->registry->template->theme = '';
+            $this->registry->template->img = '';
+            $this->registry->template->prix = '';
+            $this->registry->template->type = '';
+            $this->registry->template->titre = '';
+            $this->registry->template->date_edition = '';
+            $this->registry->template->description = '';
+            $this->registry->template->editeur = '';
+            $this->registry->template->seuil = '';
+            $this->registry->template->stock = '';
+            $this->registry->template->statut = '';
+            
+            
+            $reponse = $this->registry->db->administration_pdo->ListeDeroulanteType();
+            $reponse2 = $this->registry->db->administration_pdo->ListeDeroulanteArticle();
+            $reponse3 = $this->registry->db->administration_pdo->ListeDeroulanteArticle();
+            $this->registry->template->ttype = $reponse;
+            $this->registry->template->Titre = $reponse2;
+            $this->registry->template->TTitre = $reponse3;
+                $this->registry->template->show('administration');
+            }
+        } else {
+            $this->registry->template->show('administration');
+        }
+    }
+
+    function afficherarticle() {
+        if (isset($_SESSION['user']['user_admin'])) {
+
+            $reponse = $this->registry->db->administration_pdo->ListeDeroulanteType();
+            $reponse2 = $this->registry->db->administration_pdo->ListeDeroulanteArticle();
+            $reponse3 = $this->registry->db->administration_pdo->ListeDeroulanteArticle();
+            $this->registry->template->ttype = $reponse;
+            $this->registry->template->Titre = $reponse2;
+            $this->registry->template->TTitre = $reponse3;
+            $titrearticle = $_POST['Titre'];
+
+            $tab = $this->registry->db->administration_pdo->select_all($titrearticle);
+
+            $this->registry->template->id_article = $tab['ID_Article'];
+            $this->registry->template->theme = $tab['Theme'];
+            $this->registry->template->img = $tab['img'];
+            $this->registry->template->prix = $tab['Prix'];
+            $this->registry->template->type = $tab['Type'];
+            $this->registry->template->titre = $tab['Titre'];
+            $this->registry->template->date_edition = $tab['Date_Edition'];
+            $this->registry->template->description = $tab['Description'];
+            $this->registry->template->editeur = $tab['Editeur'];
+            $this->registry->template->seuil = $tab['Seuil'];
+            $this->registry->template->stock = $tab['Stock'];
+            $this->registry->template->statut = $tab['Statut'];
+
+
+            $this->registry->template->show('administration');
+        } else {
+            $this->registry->template->message = "Vous n'êtes pas un admin";
+            $this->registry->template->show('message');
+        }
+    }
+ function modifierarticle() {
+        if (isset($_SESSION['user']['user_admin'])) {
+            $id_article = ($_POST['ID_Article']);
+            $theme = ($_POST['Theme']);
+            $image = ($_POST['Image']);
+            $prix = ($_POST['Prix']);
+            $type = ($_POST['Type']);
+            $titre = ($_POST['Titre']);
+            $date_edition = ($_POST['Date_Edition']);
+            $description = ($_POST['Description']);
+            $editeur = ($_POST['Editeur']);
+            $seuil = ($_POST['Seuil']);
+            $stock = ($_POST['Stock']);
+            $statut = ($_POST['Statut']);
+
+            if ($theme != '' && $image != '' && $prix != '' && $type != '' && $titre != '' && $date_edition != '' && $description != '' && $editeur != '' && $seuil != '' && $stock != '' && $statut != '') {
+                $resultat = $this->registry->db->administration_pdo->UpdateArticle($id_article,$theme,$image, $prix,$type, $titre , $date_edition, $description, $editeur, $seuil, $stock, $statut);
+                
+            } else {
+                echo "<script> alert('Champs non remplis') </script>";
+                $this->registry->template->id_article = '';
+            $this->registry->template->theme = '';
+            $this->registry->template->img = '';
+            $this->registry->template->prix = '';
+            $this->registry->template->type = '';
+            $this->registry->template->titre = '';
+            $this->registry->template->date_edition = '';
+            $this->registry->template->description = '';
+            $this->registry->template->editeur = '';
+            $this->registry->template->seuil = '';
+            $this->registry->template->stock = '';
+            $this->registry->template->statut = '';
+            
+            
+            $reponse = $this->registry->db->administration_pdo->ListeDeroulanteType();
+            $reponse2 = $this->registry->db->administration_pdo->ListeDeroulanteArticle();
+            $reponse3 = $this->registry->db->administration_pdo->ListeDeroulanteArticle();
+            $this->registry->template->ttype = $reponse;
+            $this->registry->template->Titre = $reponse2;
+            $this->registry->template->TTitre = $reponse3;
+                $this->registry->template->show('administration');
+            }
+            echo "<script> alert('Succès') </script>";
+            $this->registry->template->id_article = '';
+            $this->registry->template->theme = '';
+            $this->registry->template->img = '';
+            $this->registry->template->prix = '';
+            $this->registry->template->type = '';
+            $this->registry->template->titre = '';
+            $this->registry->template->date_edition = '';
+            $this->registry->template->description = '';
+            $this->registry->template->editeur = '';
+            $this->registry->template->seuil = '';
+            $this->registry->template->stock = '';
+            $this->registry->template->statut = '';
+            
+            
+            $reponse = $this->registry->db->administration_pdo->ListeDeroulanteType();
+            $reponse2 = $this->registry->db->administration_pdo->ListeDeroulanteArticle();
+            $reponse3 = $this->registry->db->administration_pdo->ListeDeroulanteArticle();
+            $this->registry->template->ttype = $reponse;
+            $this->registry->template->Titre = $reponse2;
+            $this->registry->template->TTitre = $reponse3;
+            $this->registry->template->show('administration');
+        } else {
+            $this->registry->template->show('administration');
+        }
+    }
+    
+    function supprimerarticle() {
+        if (isset($_SESSION['user']['user_admin'])) {
+         
+                $id_article = ($_POST['ID_Article2']);
+           
+                $resultat = $this->registry->db->administration_pdo->SupprimerArticle($id_article);
+             
+                echo "<script> alert('Succès') </script>";
+                $this->registry->template->id_article = '';
+            $this->registry->template->theme = '';
+            $this->registry->template->img = '';
+            $this->registry->template->prix = '';
+            $this->registry->template->type = '';
+            $this->registry->template->titre = '';
+            $this->registry->template->date_edition = '';
+            $this->registry->template->description = '';
+            $this->registry->template->editeur = '';
+            $this->registry->template->seuil = '';
+            $this->registry->template->stock = '';
+            $this->registry->template->statut = '';
+            
+            
+            $reponse = $this->registry->db->administration_pdo->ListeDeroulanteType();
+            $reponse2 = $this->registry->db->administration_pdo->ListeDeroulanteArticle();
+            $reponse3 = $this->registry->db->administration_pdo->ListeDeroulanteArticle();
+            $this->registry->template->ttype = $reponse;
+            $this->registry->template->Titre = $reponse2;
+            $this->registry->template->TTitre = $reponse3;
+                $this->registry->template->show('administration');
+           
+
+        }
+    }
+
 }
 ?>
--- Projet_PHP/View/administration.php
+++ Projet_PHP/View/administration.php
@@ -1,7 +1,225 @@
 <?php
-
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 ?>
+
+<!DOCTYPE html>
+<style type="text/css">
+    body {
+        margin-top: 20px;
+        padding-top: 40px;
+        padding-bottom: 40px;
+        background-color: #f5f5f5;
+    }
+
+    .form-signin {
+        max-width: 300px;
+        padding: 19px 29px 29px;
+        margin: 0 auto 20px;
+        background-color: #fff;
+        border: 1px solid #e5e5e5;
+        -webkit-border-radius: 5px;
+        -moz-border-radius: 5px;
+        border-radius: 5px;
+        -webkit-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+        -moz-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+        box-shadow: 0 1px 2px rgba(0,0,0,.05);
+    }
+    .form-signin .form-signin-heading,
+    .form-signin .checkbox {
+        margin-bottom: 10px;
+    }
+    .form-signin input[type="text"],
+    .form-signin input[type="password"] {
+        font-size: 16px;
+        height: auto;
+        margin-bottom: 15px;
+        margin-right: 10px;
+        padding: 7px 9px;
+    }
+
+</style>
+
+</head>
+
+<body>
+
+    <div class="modal-form container">
+        <center>
+            <table>
+                <tr>
+                    <td>
+                        
+                    </td>
+                    <td>
+
+
+
+           
+                        
+                        <form class="form-signin" action="<?php echo __SITE_URL ?>/administration/ajoutarticle" method="POST" name="formAjoutArticle" style="display:inline-block;">
+                            <h2 class="form-signin-heading">Ajouter un article</h2>
+                            Thème
+                            <input type="text"  class="input-block-level" placeholder="Thème" name="Theme">    
+                            Image
+                            <input type="text"  class="input-block-level" placeholder="Image" name="Image">
+                            Prix
+                            <input type="text"  class="input-block-level" placeholder="Prix" name="Prix">
+                            Type 
+                            
+                            </br>
+                            
+                            <!--<input type="text"  class="input-block-level" placeholder="Type" name="Type">-->
+
+                 
+                            <select id="Type" name="Type">
+                                <?php
+// On affiche chaque entrée une à une
+                                foreach($ttype as $key => $donnees)
+                                {
+                                    echo '<option selected value="' . $donnees['type'] . '">' . $donnees['type'] . '</option>'; //ou bien $donnees['monchamp']
+                                    
+                                }
+                                
+                                ?>
+                            </select>
+                       
+                            </br>
+                            Titre
+                            <input type="text"  class="input-block-level" placeholder="Titre" name="Titre">
+                            Date d'édition
+                            <input type="date"  class="input-block-level" placeholder="Date d'édition" name="Date_Edition">
+                            Description
+                            <input type="text"  class="input-block-level" placeholder="Description" name="Description">     
+                            Editeur
+                            <input type="text"  class="input-block-level" placeholder="Editeur" name="Editeur">
+                            Seuil
+                            <input type="text"  class="input-block-level" placeholder="Seuil" name="Seuil">
+                            Stock
+                            <input type="text"  class="input-block-level" placeholder="Stock" name="Stock">
+                            Statut
+                            <input type="text"  class="input-block-level" placeholder="Statut" name="Statut">
+
+                            <p></p>
+                            <button class="btn btn-large btn-primary" type="submit" >Ajouter</button>
+                        </td>
+                        </form>  
+                    </div>
+                    <div class ="form-signin">
+                        <h2 class="form-signin-heading">Choisir un article</h2>
+                        <form  action="<?php echo __SITE_URL ?>/administration/afficherarticle" method="POST" id="formList2" style="display:inline-block;">
+                            <select onchange="document.getElementById('formList2').submit()" id="Titre" name="Titre">
+                                <?php
+// On affiche chaque entrée une à une
+                                foreach($Titre as $keyy => $donneess)
+                                {
+                                    echo '<option value="' . $donneess['Titre'] . '">' . $donneess['Titre'] . '</option>'; //ou bien $donnees['monchamp']
+                                    
+                                }
+                             
+                                ?>
+                                </select>
+                        </form>
+             
+                    </div>
+                   
+                    <td>
+                      <div class="form-signin">
+                         <h2 class="form-signin-heading">Modifier un article</h2>
+                         
+                        <form action="<?php echo __SITE_URL ?>/administration/modifierarticle" method="POST" name="formModifierArticle" style="display:inline-block;">
+                            
+                            <input  name="ID_Article" id="ID_Article" type="text"  class="input-block-level" placeholder="ID" value="<?php echo $id_article; ?>">
+                            Thème
+                            <input type="text"  class="input-block-level" placeholder="Thème" name="Theme" value="<?php echo $theme; ?>">    
+                            Image
+                            <input type="text"  class="input-block-level" placeholder="Image" name="Image" value="<?php echo $img; ?>">
+                            Prix
+                            <input type="text"  class="input-block-level" placeholder="Prix" name="Prix" value="<?php echo $prix; ?>">
+                            Type
+                            </br>
+                            
+                            <select style="width: 50%;" id="Type" name="Type">
+                                <?php
+// On affiche chaque entrée une à une
+                                foreach($ttype as $key => $donnees)
+                                {
+                                    echo '<option value="' . $donnees['type'] . '">' . $donnees['type'] . '</option>'; //ou bien $donnees['monchamp']
+                                    
+                                }
+                                
+                                ?>
+                            </select>
+                            
+                                <input type="text" style="width: 40%"  class="input-block-level" placeholder="Type" name="Type" disabled value="<?php echo $type;?>">
+                            Titre
+                            
+                            <input type="text"  class="input-block-level" placeholder="Titre" name="Titre" value="<?php echo $titre; ?>">
+                            Date d'édition
+                            <input type="date"  class="input-block-level" placeholder="Date d'édition" name="Date_Edition" value="<?php echo $date_edition; ?>">
+                            Description
+                            <input type="text"  class="input-block-level" placeholder="Description" name="Description" value="<?php echo $description; ?>">     
+                            Editeur
+                            <input type="text"  class="input-block-level" placeholder="Editeur" name="Editeur" value="<?php echo $editeur; ?>">
+                            Seuil
+                            <input type="text"  class="input-block-level" placeholder="Seuil" name="Seuil" value="<?php echo $seuil; ?>">
+                            Stock
+                            <input type="text"  class="input-block-level" placeholder="Stock" name="Stock" value="<?php echo $stock; ?>">
+                            Statut
+                            <input type="text"  class="input-block-level" placeholder="Statut" name="Statut" value="<?php echo $statut ; ?>">
+
+                         
+                            <button class="btn btn-large btn-primary" type="submit" >Modifier</button>
+                        </form>  
+                      </div>
+                    </td>
+                    <td>
+                        <div class="form-signin">
+                         <h2 class="form-signin-heading">Supprimer un article</h2>
+                         
+                        <form action="<?php echo __SITE_URL ?>/administration/supprimerarticle" method="POST" name="formSupprimerArticle" style="display:inline-block;">
+                          
+                            <input  name="ID_Article2" id="ID_Article2"  type="text"  class="input-block-level" placeholder="ID"  value="<?php echo $id_article; ?>">
+                            Thème
+                            <input type="text"  class="input-block-level" placeholder="Thème" name="STheme" disabled value="<?php echo $theme; ?>">    
+                            Image
+                            <input type="text"  class="input-block-level" placeholder="Image" name="Image" disabled value="<?php echo $img; ?>">
+                            Prix
+                            <input type="text"  class="input-block-level" placeholder="Prix" name="Prix" disabled value="<?php echo $prix; ?>">
+                            Type
+                            </br>
+                            
+                            <input type="text"  class="input-block-level" placeholder="Type" name="Type" disabled disabled value="<?php echo $type;?>">
+                            Titre
+                            
+                            <input type="text"  class="input-block-level" placeholder="Titre" name="Titre" disabled value="<?php echo $titre; ?>">
+                            Date d'édition
+                            <input type="date"  class="input-block-level" placeholder="Date d'édition" disabled name="Date_Edition" value="<?php echo $date_edition; ?>">
+                            Description
+                            <input type="text"  class="input-block-level" placeholder="Description" disabled name="Description" value="<?php echo $description; ?>">     
+                            Editeur
+                            <input type="text"  class="input-block-level" placeholder="Editeur" disabled name="Editeur" value="<?php echo $editeur; ?>">
+                            Seuil
+                            <input type="text"  class="input-block-level" placeholder="Seuil" disabled name="Seuil" value="<?php echo $seuil; ?>">
+                            Stock
+                            <input type="text"  class="input-block-level" placeholder="Stock" disabled name="Stock" value="<?php echo $stock; ?>">
+                            Statut
+                            <input type="text"  class="input-block-level" placeholder="Statut" disabled name="Statut" value="<?php echo $statut ; ?>">
+
+                         
+                            <button class="btn btn-large btn-primary" type="submit" >Supprimer</button>
+                        </form>  
+                      </div>
+                    </td>
+                </tr>
+            </table>
+        </center>
+
+<script>
+document.getElementById("ID_Article").style.display = 'none';
+document.getElementById("ID_Article2").style.display = 'none';
+</script>
+
+    </div> <!-- /container -->
