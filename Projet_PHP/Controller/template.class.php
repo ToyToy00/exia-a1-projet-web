8da5548bb8a41a9019da202a347cb4f0a2bdb18b
valentin.lafranca <valentin.lafranca@gmail.com>
2013-06-26 09:12:07 +0000

    fix count panier

--- Projet_PHP/Controller/template.class.php
+++ Projet_PHP/Controller/template.class.php
@@ -18,7 +18,7 @@ class template {
         $path_header = __SITE_PATH . '/View/header.php';
         $path_footer = __SITE_PATH . '/View/footer.php';
         $path = __SITE_PATH . '/View' . '/' . $name . '.php';
-        
+
         if (!isset($_SESSION['user']['user_info'])) {
             $this->registry->template->connect_nav = '<form class="navbar-form pull-right" action="' . __SITE_URL . '/authentification/login" method="POST" >
                         <input type="text" class="txtbx_login" placeholder="Nom d\'utilisateur" name="comail">
@@ -27,34 +27,35 @@ class template {
                         <button type="button" class="submit_login btn-primary btn_panier" class="btn" onclick="location.href=\'' . __SITE_URL . '/authentification/\'">Pas encore Inscrit ?</button>
                         </form>';
             $this->registry->template->nb_article = "Veuillez vous connecter pour ajouter des";
-            
         } else {
             $connect_nav = '<div class="navbar-form pull-right">
                 <p> Bienvenue ' . $_SESSION['user']['user_info'] . ' 
                     
                         <button class="submit_login btn-primary btn_panier" class="btn" onclick="location.href=\'' . __SITE_URL . '/authentification/logout\'">Déconnexion</button>
                  ';
-         
-            /*if(isset($_SESSION['user']['user_admin']))
-            {
-                $connect_nav .= '<button class="submit_login btn-primary btn_panier" class="btn" onclick="location.href=\'' . __SITE_URL . '/administration/\'">Administration</button>';
-            }*/
-           $connect_nav .= '</p></div>';
-          
-           $this->registry->template->connect_nav = $connect_nav;
-           $count = '';
-           if(isset($_SESSION['user']['panier']))
-           {
-           $count = count($_SESSION['user']['panier']);
 
-           if(isset($_SESSION['user']['panier']['total']))
-           {
-               $temp =  $count;
-               $count = $temp - 1;
-           }
-           }
-           $this->registry->template->nb_article = $count;
+            /* if(isset($_SESSION['user']['user_admin']))
+              {
+              $connect_nav .= '<button class="submit_login btn-primary btn_panier" class="btn" onclick="location.href=\'' . __SITE_URL . '/administration/\'">Administration</button>';
+              } */
+            $connect_nav .= '</p></div>';
+
+            $this->registry->template->connect_nav = $connect_nav;
+            $count = '';
+            if (isset($_SESSION['user']['panier'])) {
+                $count = count($_SESSION['user']['panier']);
+
+                if (isset($_SESSION['user']['panier']['total'])) {
+                    $temp = $count;
+                    $count = $temp - 1;
+                }
+                $this->registry->template->nb_article = $count;
+            } else {
+                $this->registry->template->nb_article = 0;
+            }
         }
+
+        
         if (file_exists($path) == false) {
             throw new Exception('Template not found in ' . $path);
             return false;
@@ -71,13 +72,14 @@ class template {
         include ($path);
         include ($path_footer);
     }
-function showu($name) {
+
+    function showu($name) {
         $path_header = __SITE_PATH . '/View/header.php';
         $path_footer = __SITE_PATH . '/View/footer.php';
         $path = __SITE_PATH . '/View' . '/' . $name . '.php';
-        
-        
-        
+
+
+
         if (file_exists($path) == false) {
             throw new Exception('Template not found in ' . $path);
             return false;
@@ -87,8 +89,11 @@ function showu($name) {
         foreach ($this->vars as $key => $value) {
             $$key = $value;
         }
-        include ($path);     
+        include ($path);
     }
+
 }
+
+
 
 ?>
